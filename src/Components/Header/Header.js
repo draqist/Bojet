49e02d9a9c0b4e7819539b73ec5629eea8f5f5e9
Draqode <Boomjay008@gmail.com>
2022-01-01 14:07:29 +0100

    subtle changes

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,22 +1,23 @@
 import React from 'react'
+import './header.scss'
 
 const Header = () => {
   return (
-    <div className = 'px-10 h-20 flex items-center justify-between'>
-      <div className='flex items-center justify-between flex-grow logo'>
-        <h3 className='font-semibold  text-4xl text-gray-00 tracking-wide mr-4 text-center'>Bujet</h3>
+    <div className = 'header'>
+      <div className='logo'>
+        <h3 className=''>Bujet</h3>
         <nav >
-          <ul className = 'flex items-center text-center justify-end'>
-            <li className = 'px-10  font-semibold text-gray-400 text-lg '> Plan </li>
-            <li className = 'px-6 font-semibold text-gray-400 text-lg' > Save </li>
-            <li className = 'px-6 font-semibold text-gray-400 text-lg' > Invest </li>
+          <ul className = ''>
+            <li className = ''> Plan </li>
+            <li className = '' > Save </li>
+            <li className = '' > Invest </li>
           </ul>
         </nav>
       </div>
       <div>
         
       </div>
-      <div className='btn-cta flex-none '>
+      <div className='btn-cta'>
         <button className = 'border h-10 px-4 hover:text-indigo-50 hover:bg-blue-800 hover:ease-out duration-200 hover:shadow-lg text-center rounded border-blue-800 font-normal text-gray-700'> Log In </button>
         <button className = 'ml-5 px-6 h-10 text-center bg-blue-800  text-white rounded text-base font-normal'> Sign up for free  </button>
       </div>
--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -5,40 +5,41 @@ import support from '../../Icons/support.png'
 import product from '../../Icons/Product.png'
 import cost from '../../Icons/cost.png'
 import delivery from '../../Icons/delivery.png'
-import './land.css'
+import './land.scss'
 
 const LandingPage = () => {
   return (
-    <div className = ' bg-white'>
+    <div className = 'land '>
       <Header />
-      <main className='hero px-10 flex items-center justify-evenly mt-10'>
-        <div className='heroText text-6xl leading-tight text-blue-800 font-bold mt-16 mb-24 '>
-          Track your <span className = 'text-yellow-600'>expenses,</span> <br />
-          Budget your <span className='text-yellow-600'>incomes</span> and <span className='text-yellow-600'>save money.</span>
-          <span className='block text-gray-300 text-lg pt-1'>We help you automate settling your expenses and budgets.</span>
-          <button className = 'bg-blue-600 text-center text-lg w-40 h-12 text-white rounded-lg'> Get started </button>
+      <main className='hero'>
+        <div className='heroText'>
+          Track your <span>expenses,</span> <br />
+          Budget your <span>incomes</span> and <span>save money.</span>
+          <span className='block'>We help you automate settling your expenses and budgets.</span>
+          <button className = 'btn'> Get started </button>
         </div>
         <img src={ hero } alt='hero' />
       </main>
-      <div className = 'mt-24'>
+      <div className = 'prod mt-24'>
         <img src={product} alt = 'product' className = 'mt-24'/>
       </div>
-      <main className='overview mt-24 flex justify-around items-center text-blue-900'>
+      <main className='overview mt-24 flex  text-blue-900'>
         <img src={support} alt='overviewImg' className = 'sup' style = {{width: '31rem'}} />
         <div className='herotext text-5xl leading-tight text-blue-800 font-bold mt-16 '>
-          <span className='block font-light text-gray-400 uppercase text-xl pt-1 pb-8'>always online</span>
-          Real-time support with <span className='text-yellow-600' >Firestore</span>
-          <span className='block text-gray-300 text-sm pt-1'>
+          <span className='block font-light text-gray-400 capitalize text-xl pt-1 pb-4'>always online</span>
+          Real-time support with <span className='textSpan text-yellow-600' >Firestore</span>
+          <span className='blocks text-gray-300 text-sm pt-1'>
             Amet ad duis ea officia irure ipsum irure dolore dolore nostrud. Est aliquip duis consequat nulla sit do. Adipisicing
           </span>
         </div>
       </main>
       <main className='overview mt-36 flex justify-around items-center text-blue-900'>
         <div className='herotext text-5xl leading-tight text-blue-800 font-bold mt-16 '>
-          <span className='block font-light text-gray-400 uppercase text-xl pt-1 pb-8'>free some cost</span>
-            <span className='text-yellow-600' >Save</span> cost for you and family
-          <span className='block text-gray-300 text-sm pt-1'>
-            Amet ad duis ea officia irure ipsum irure dolore dolore nostrud. Est aliquip duis consequat nulla sit do. Adipisicing
+          <span className='block font-light text-gray-400 capitalize text-xl pt-1 pb-4'>free some cost</span>
+            <span className='textSpan' >Save</span> cost for you and family
+          <span className='blocks'>
+            Save for the rainy day
+          <br/> Build a safety net for unexpected expenses
           </span>
         </div>
         <img src={cost} alt='overviewImg' className = 'sup' style = {{width: '31rem'}} />
@@ -46,9 +47,9 @@ const LandingPage = () => {
       <main className='overview mt-36 flex justify-around items-center text-blue-900'>
         <img src={delivery} alt='overviewImg' className = 'sup' style = {{width: '31rem'}} />
         <div className='herotext text-5xl leading-tight text-blue-800 font-bold mt-16 '>
-          <span className='block font-light text-gray-400 uppercase text-xl pt-1 pb-8'>Use anytime </span>
-            <span className='text-yellow-600' >Easily</span> accessible and secure for use.
-          <span className='block text-gray-300 text-sm pt-1'>
+          <span className='block font-light text-gray-400 capitalize text-xl pt-1 pb-4'>Use anytime </span>
+            <span className='textSpan' >Easily</span> accessible and secure for use.
+          <span className='blocks'>
             Amet ad duis ea officia irure ipsum irure dolore dolore nostrud. Est aliquip duis consequat nulla sit do. Adipisicing
           </span>
         </div>
